6f1a46b3e9d5bb1cbed0e42b432061c164a4d0c6
stormor <stormor@naver.com>
2024-09-17 18:06:29 +0900

    second

--- src/components/bottom.tsx
+++ src/components/bottom.tsx
@@ -14,11 +14,11 @@ function Home() {
         </div>
         <div className='bottom-main'>
           <div className='bottom-main-cell'>
-            (06158) 서울특별시 강남구 테헤란로79길 6, 4층 브이 565호 주식회사 서베이탑<br />
+            (06158) 서울특별시 강남구 테헤란로, 565호 주식회사 서베이<br />
 
-            대표이사 : 윤종현   |   사업자번호 : 372-88-02352   |   대표전화 : 02-533-2188   |   이메일 : support@surveytop.co.kr
+            대표이사 : 홍길동  |   사업자번호 : 372-11-12345   |   대표전화 : 02-533-1234   |   이메일 : support@survey.co.kr
             <br />
-            Copyright © 주식회사 서베이탑  All rights reserved.
+            Copyright © 주식회사 서베이  All rights reserved.
           </div>
 
 
